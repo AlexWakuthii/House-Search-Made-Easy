35b23904e8abfd52558472d87f52639c029895af
AlexWakuthi <muriithi1734@gmail.com>
2023-07-20 15:54:07 +0300

    added header and footer files

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// footer section
+  // Fetch the footer.html content and insert it into the footer-placeholder div
+  fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
 
+// Fetch the header.html content and insert it into the header-placeholder div
+fetch('header.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('header-placeholder').innerHTML = data;
+});
